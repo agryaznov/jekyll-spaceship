f19a90caa39a4ae43b0e1a132d10201a586e4373
agryaznov <adgryaznov@gmail.com>
2020-05-25 21:13:59 +0300

    turned off all processors except tables-processor

--- lib/jekyll-spaceship.rb
+++ lib/jekyll-spaceship.rb
@@ -8,9 +8,9 @@ require 'jekyll-spaceship/cores/register'
 module Jekyll::Spaceship
   Logger.display_info
   Register.use 'table-processor'
-  Register.use 'mathjax-processor'
-  Register.use 'plantuml-processor'
-  Register.use 'polyfill-processor'
-  Register.use 'video-processor'
-  Register.use 'emoji-processor'
+  #Register.use 'mathjax-processor'
+  #Register.use 'plantuml-processor'
+  #Register.use 'polyfill-processor'
+  #Register.use 'video-processor'
+  #Register.use 'emoji-processor'
 end
